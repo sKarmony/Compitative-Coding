62a53a4791cd81b9fbdc973ef88a4fe8b661aa44
sKarmony <rishabh.179302112@muj.manipal.edu>
2020-05-07 18:32:56 +0530

    Edited vector file

--- vector.cpp
+++ vector.cpp
@@ -2,6 +2,6 @@
 /*
 vector also has the same properties(methods) as arrays
 extras:
-push_back(10) adds 10 in the list
+push_back(9) adds 9 in the list
 pop_back() removes last elemnt from the vector
 */
